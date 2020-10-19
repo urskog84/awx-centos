52d6a86b34d9b19fe003ac79e689ed0016c348be
Petter Fors <karl.petter.fors@gmail.com>
2020-10-19 21:38:48 +0200

    fix som bug in config section in firewall config

--- install_awx_manage.sh
+++ install_awx_manage.sh
@@ -1,3 +1,8 @@
+# Variables section
+
+dns_servers="8.8.8.8,8.8.4.4"
+admin_password="passw0rd"
+
 # pws
 pwd=$(pwd)
 
@@ -23,20 +28,19 @@ alternatives --set python /usr/bin/python3
 
 # clone AWX
 echo "clone awx"
-[ ! -f $pwd/awx] && git clone https://github.com/ansible/awx.git -q
+[ ! -f $pwd/awx ] && git clone https://github.com/ansible/awx.git -q
 
 # Config inventory 
-if [ -f $pwd/awx/installer/inventory/conf_done]; then
+if [ -f $pwd/awx/installer/conf_done ]; then
     echo "skipping inventory confg"
 else
     secret_key=$(openssl rand -base64 30)
-    dns_servers="8.8.8.8,8.8.4.4"
-    admin_password=passw0rd
-    
+
     sudo sed -i "/secret_key=/c\secret_key=$secret_key" awx/installer/inventory 
     sudo sed -i "/awx_alternate_dns_servers=/c\awx_alternate_dns_servers=$dns_servers" awx/installer/inventory 
-    sudo sed -i "/admin_password=/c\admin_password=$admin_passowrd" awx/installer/inventory 
-    touch $pwd/awx/installer/inventory/conf_done
+    sudo sed -i "/admin_password=/c\admin_password=$admin_password" awx/installer/inventory 
+
+    touch $pwd/awx/installer/conf_done
 fi
 
 [ ! -f /var/lib/pgdocker ] && sudo mkdir /var/lib/pgdocker
@@ -45,11 +49,12 @@ fi
 ansible-playbook -i $pwd/awx/installer/inventory $pwd/awx/installer/install.yml
 
 
+if [[ ! $(sudo firewall-cmd --list-all | grep services) = *http* ]]; then
+    sudo firewall-cmd --zone=public --add-masquerade --permanent
+    sudo firewall-cmd --permanent --add-service=http
+    sudo firewall-cmd --permanent --add-service=https
+    sudo firewall-cmd --reload
+fi
 
-sudo firewall-cmd --zone=public --add-masquerade --permanent
-sudo firewall-cmd --permanent --add-service=http
-sudo firewall-cmd --permanent --add-service=https
-sudo firewall-cmd --reload
-
+# Disabel Selinux
 sudo sed -i "/SELINUX=enforcing/c\SELINUX=disabled" /etc/sysconfig/selinux 
-~
