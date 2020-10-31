071d99a58ac8c760590bfdcdb8f1a2e1fdd76f56
Petter Fors <karl.petter.fors@gmail.com>
2020-10-31 20:35:39 +0100

    Chnage to Centos7

--- install_awx_manage.sh
+++ install_awx_manage.sh
@@ -5,30 +5,37 @@ admin_password="passw0rd"
 
 # pws
 pwd=$(pwd)
-
+subdir=/awx
+clone_dir=$pwd$subdir
 # install depndencys
-sudo dnf install epel-release -y
-sudo dnf install git gcc gcc-c++ ansible nodejs gettext device-mapper-persistent-data lvm2 bzip2 python3-pip -y
-sudo dnf install langpacks-en glibc-all-langpacks -y
-sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install epel-release -y
+sudo yum install git gcc gcc-c++ ansible nodejs gettext device-mapper-persistent-data lvm2 bzip2 python3-pip libselinux-python3 -y
+sudo yum install langpacks-en glibc-all-langpacks -y
+
 
 #install docker
-docker_ver=$(dnf list docker-ce  --showduplicates | grep docker-ce. | awk '{print $2}')
-sudo dnf install docker-ce-$docker_ver -y
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker -y
 
 # Start docker
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
-sudo pip3 install docker-compose
+newgrp docker 
+sudo pip3 install docker-compose 
 
 # switch to python 3
 alternatives --set python /usr/bin/python3
 
 # clone AWX
-echo "clone awx"
-[ ! -f $pwd/awx ] && git clone https://github.com/ansible/awx.git -q
+echo "clone awx to $pwd/awx"
+if [ -d $pwd/awx ]; then
+    echo  "awx allredy cloned"
+else
+    echo  "start cloning awx"
+    git clone https://github.com/ansible/awx.git -q
+fi
 
 # Config inventory 
 if [ -f $pwd/awx/installer/conf_done ]; then
@@ -43,10 +50,12 @@ else
     touch $pwd/awx/installer/conf_done
 fi
 
-[ ! -f /var/lib/pgdocker ] && sudo mkdir /var/lib/pgdocker
+if [ ! -d /var/lib/pgdocker ]; then
+    sudo mkdir /var/lib/pgdocker
+fi
 
 # Start AWX install
-ansible-playbook -i $pwd/awx/installer/inventory $pwd/awx/installer/install.yml
+sudo ansible-playbook -i $pwd/awx/installer/inventory $pwd/awx/installer/install.yml
 
 
 if [[ ! $(sudo firewall-cmd --list-all | grep services) = *http* ]]; then
@@ -57,4 +66,6 @@ if [[ ! $(sudo firewall-cmd --list-all | grep services) = *http* ]]; then
 fi
 
 # Disabel Selinux
-sudo sed -i "/SELINUX=enforcing/c\SELINUX=disabled" /etc/sysconfig/selinux 
+sudo sed -i "/SELINUX=enforcing/c\SELINUX=disabled" /etc/selinux/config
+
+sudo reboot
